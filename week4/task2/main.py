3a1d9303cce1e9293f2589fa4135a8b45c95f8a6
kalkidan yishak <134784719+kalkidanyishakz@users.noreply.github.com>
2023-12-12 02:53:26 +0300

    Update main.py
    fixed a bug that makes a the script it self to move to a sub directory

--- week4/task2/main.py
+++ week4/task2/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 '''
 functions
     list_files
@@ -12,12 +13,46 @@ def list_files():
     for a_file in os.scandir():
         if a_file.is_file():
             files.append(a_file.name)
-        
+
     return files
 
 def created_or_modified_24(my_file):
     my_path=os. getcwd()+'/'+my_file
     crtd=os.stat(my_path).st_ctime
     mdfd=os.stat(my_path).st_mtime
+    now=time.time()
 
-created_or_modified_24('main.py')
+    crtd_hr=(now-crtd)/3600
+    mdfd_hr=(now-mdfd)/3600
+    print(str(now)+' '+str(crtd)+' '+str(crtd_hr)+' '+ my_file)
+
+    if crtd_hr<=24 or mdfd_hr<=24:
+        return True
+    return False
+
+def append_time_stamp(my_file):
+    file = open(my_file, "a")
+    file.write(str(time.time())+'\n')
+    file.close()
+
+def create_folder():
+    import os
+    path = "last_24hours"
+    if os.path.isdir(path):
+        print("The folder already exists")
+    else:
+        os.mkdir(path)
+
+
+def move_files():
+    cwd = os.getcwd()
+    create_folder()
+    files=list_files()
+    for file in files:
+        if created_or_modified_24(file) and file != 'main.py':
+            append_time_stamp(file)
+            src = os.path.join(cwd, file)
+            dst = os.path.join(cwd, "last_24hours", file)
+            os.rename(src, dst)
+
+move_files()
